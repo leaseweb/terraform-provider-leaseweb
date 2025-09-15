317c27e1336990f3cca6b0224001386b474925f5
Imtiaz Ahmed <ippabel@gmail.com>
2025-09-15 11:01:01 +0200

    docs(dns_resource_record_set):  Update import docs (#428)
    Co-authored-by: Imtiaz Ahmed <i.ahmed@global.leaseweb.com>

---
+++ examples/resources/leaseweb_dns_resource_record_set/import.sh
@@ -0,0 +1,2 @@
+# A DNS record can be imported by specifying the domain name, record name and record type in comma-separated format.
+terraform import leaseweb_dns_resource_record_set.example "example.com,www.example.com,A"
